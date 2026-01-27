9a2e287f4b4bf77bc1c769834ad290315daa7826
divijg19 <divijganjoo2003@gmail.com>
2026-01-27 21:00:00 +0530

    v0.0.4: crates/renderer/ API bindings and pub wiring

--- crates/renderer/src/lib.rs
+++ crates/renderer/src/lib.rs
@@ -1,4 +1,23 @@
-/// Minimal renderer crate placeholder
+use juniper_layout::PhysicalDoc;
+
+pub trait Renderer {
+    fn render(&self, doc: &PhysicalDoc) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+pub struct PrintRenderer;
+
+impl Renderer for PrintRenderer {
+    fn render(&self, doc: &PhysicalDoc) -> Result<(), Box<dyn std::error::Error>> {
+        for b in &doc.boxes {
+            println!(
+                "BOX @({:.1},{:.1}) {:.1}x{:.1}: {}",
+                b.x, b.y, b.w, b.h, b.content
+            );
+        }
+        Ok(())
+    }
+}
+
 pub fn version() -> &'static str {
     "0.1.0"
 }
